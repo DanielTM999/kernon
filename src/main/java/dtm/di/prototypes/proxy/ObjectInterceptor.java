0caacc06630b8825030d9dd428fcb43913630594
DanielTM999 <danielmelo101203@gmail.com>
2026-05-20 00:05:40 -0300

    .

--- src/main/java/dtm/di/prototypes/proxy/ObjectInterceptor.java
+++ src/main/java/dtm/di/prototypes/proxy/ObjectInterceptor.java
@@ -8,58 +8,50 @@ import net.bytebuddy.implementation.bind.annotation.*;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Optional;
 import java.util.concurrent.Callable;
 
 public class ObjectInterceptor {
-    private final Object delegate;
+    private final Object realInstance;
     private final DependencyContainer dependencyContainer;
 
-    ObjectInterceptor(Object delegate, DependencyContainer dependencyContainer){
+    ObjectInterceptor(Object realInstance, DependencyContainer dependencyContainer){
         this.dependencyContainer = dependencyContainer;
-        this.delegate = delegate;
+        this.realInstance = realInstance;
     }
 
     @RuntimeType
     public Object intercept(
             @Origin Method method,
             @AllArguments Object[] args,
-            @This Object proxy
+            @This Object proxy,
+            @SuperCall Callable<?> zuper
     ) throws Throwable  {
         final AopUtils aopUtils = AopProxyUtils.getInstance(dependencyContainer);
-        final Method realMethod = getRealMethodFromDelegate(method);
 
-        aopUtils.applyBefore(realMethod, args, proxy, delegate);
+        aopUtils.applyBefore(method, args, proxy, realInstance);
         try {
             Object result;
             try{
-                result = realMethod.invoke(delegate, args);
-            }catch (IllegalAccessException | InvocationTargetException reflectiveOperationException){
+                result = zuper.call();
+            }catch (RuntimeException | Error directThrow){
+                throw directThrow;
+            }catch (InvocationTargetException invocationTargetException){
+                throw new AopMainMethodException("Erro ao invocar main AOP Method", invocationTargetException);
+            }catch (Throwable reflectiveOperationException){
                 throw new AopMainMethodException("Erro ao invocar main AOP Method", reflectiveOperationException);
             }
-            return aopUtils.applyAfter(realMethod, args, proxy, delegate, result);
+            return aopUtils.applyAfter(method, args, proxy, realInstance, result);
         }catch (AopMainMethodException aopMainMethodException){
             Throwable cause = extractRootError(aopMainMethodException.getCause());
-            executeOnErrorOrThrow(aopUtils, cause, realMethod, args, proxy, delegate);
+            executeOnErrorOrThrow(aopUtils, cause, method, args, proxy, realInstance);
             throw cause;
         }catch (RuntimeException runtimeException){
-            Throwable cause = runtimeException.getCause();
-            throw extractRootError(cause);
+            Throwable cause = (runtimeException.getCause() != null) ? runtimeException.getCause() : runtimeException;
+            Throwable root = extractRootError(cause);
+            executeOnErrorOrThrow(aopUtils, root, method, args, proxy, realInstance);
+            throw root;
         }
 
-    }
-
-    private Method getRealMethodFromDelegate(Method proxyMethod) {
-        try {
-            return delegate.getClass().getMethod(proxyMethod.getName(), proxyMethod.getParameterTypes());
-        } catch (NoSuchMethodException e) {
-            try {
-                return delegate.getClass()
-                        .getDeclaredMethod(proxyMethod.getName(), proxyMethod.getParameterTypes());
-            } catch (NoSuchMethodException ex) {
-                return proxyMethod;
-            }
-        }
     }
 
     private void executeOnErrorOrThrow(
